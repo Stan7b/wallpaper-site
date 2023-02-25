f8aaaf7d4da8ca4660d8808b4eb618b185b763ea
Stan7b <Moder126119>
2023-02-25 13:34:32 +0200

    First commit

---
+++ src/main/java/net/stan7b/wallpapersite/WallpaperSiteApplication.java
@@ -0,0 +1,13 @@
+package net.stan7b.wallpapersite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallpaperSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallpaperSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/stan7b/wallpapersite/WallpaperSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.stan7b.wallpapersite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallpaperSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
